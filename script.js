1e511d8f19f34bbc6f5a79d80fc230b6dec49795
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-14 11:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	const sArray = s.split(" ");
+	return sArray[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s))
